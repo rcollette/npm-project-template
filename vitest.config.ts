c405320380bb6cecd9272eb766e7c3dbeb2c7d3f
Rich Collette <Richard.Collette@MacBook-Pro-3.local>
2025-07-17 18:11:45 -0400

    add more report types, coverage thresholds, use .spec.ts for unit test extension

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,11 +4,21 @@ export default defineConfig({
   test: {
     globals: true, // or true if you prefer `test()`/`expect()` globals
     environment: "node",
-    include: ["tests/**/*.test.ts"],
+    include: ["tests/**/*.spec.ts"],
     setupFiles: ["vitest.setup.ts"],
+    reporters: "junit",
+    outputFile: {
+      junit: "./reports/junit/results.xml", // Path to the JUnit report file
+    },
     coverage: {
-      reporter: ["text", "lcov", "html"],
+      reporter: ["text", "lcov", "cobertura", ""],
       reportsDirectory: "./reports/coverage",
+      thresholds: {
+        lines: 100, // require ≥ 80% lines covered
+        functions: 100, // require ≥ 75% functions covered
+        branches: 100, // require ≥ 70% branches covered
+        statements: 100, // require ≥ 80% statements covered
+      },
     },
     typecheck: {
       enabled: true,
